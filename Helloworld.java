f9237940ac61cc9fca0255ae3a59455edd0395ff
Jayesh S <Jayesh S>
2017-03-23 11:15:34 +0800

    Test

--- Helloworld.java
+++
@@ -1,7 +0,0 @@
-$ vim helloworld.java
-/* Hello World Java Program */
-class helloworld  {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
--- helloworld_v1.java
+++
@@ -1,6 +0,0 @@
-/* Hello World Java Program */
-class helloworld_v1  {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
